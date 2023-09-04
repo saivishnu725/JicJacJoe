93cb926fd3b21a21ffbf14dfab0bd2e37c19f5a5
Sai Vishnu <saivishnu725@gmail.com>
2023-09-04 21:41:23 +0530

    no idea why the logic isnt working.

--- console/JicJacJoe.java
+++ console/JicJacJoe.java
@@ -29,6 +29,8 @@ public class JicJacJoe {
     // score for p1 and p2
     static int[] score = { 0, 0 };
 
+    static boolean won = false;
+
     public static void main(String[] args) throws IOException {
 
         // input bs
@@ -45,7 +47,6 @@ public class JicJacJoe {
 
         // first time
         printBoard();
-        System.out.print(" Choose a position(1-9, Q/q to exit):  ");
         input = read.readLine();
 
         while (true) {
@@ -87,6 +88,10 @@ public class JicJacJoe {
 
             }
 
+            if (logic()) {
+                System.out.println("Exiting...");
+                break;
+            }
             printBoard();
             input = read.readLine();
 
@@ -107,6 +112,60 @@ public class JicJacJoe {
         } else {
             invalidInput = true;
         }
+    }
+
+    private static boolean logic() {
+        for (int i = 1; i < 10; i++) {
+            System.out.print(values[i] + " ");
+        }
+
+        // int[][] possibilities = {
+        // { 1, 5, 9 },
+        // { 7, 5, 3 },
+        // { 1, 2, 3 },
+        // { 4, 5, 6 },
+        // { 7, 8, 9 },
+        // { 1, 4, 7 },
+        // { 2, 5, 8 },
+        // { 3, 6, 9 }
+        // };
+
+        // for (int i = 0; i < 8; i++) {
+
+        // int curr = current.equals("X") ? 1 : 2;
+        // int pos1 = values[possibilities[i][0]];
+        // int pos2 = values[possibilities[i][1]];
+        // int pos3 = values[possibilities[i][2]];
+        // if (pos1 == curr &&
+        // pos2 == curr &&
+        // pos3 == curr)
+        // System.out.println("hello");
+
+        // score[0]++;
+        // win
+        // }
+
+        // diagonal
+        if ((values[0] == values[5] && values[0] == values[9]) || (values[3] == values[5] && values[3] == values[7])) {
+            won = true;
+        } else {
+            // horizontal
+            for (int i = 1; i < 9; i += 3) {
+                if ((values[i] == values[i + 1] && values[i] == values[i + 2])){
+                    System.out.println("True 1");
+                    return true;
+                    
+                }
+            }
+            // vertical
+            for (int i = 1; i < 3; i++) {
+                if ((values[i] == values[i + 3] && values[i] == values[i + 6])){
+                    System.out.println("true 2");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private static void clearScreen() {
