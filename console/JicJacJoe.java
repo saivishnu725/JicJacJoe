220be3cdf6bee72761976094f4dfc10ee8a9a069
Sai Vishnu <saivishnu725@gmail.com>
2023-09-04 16:25:47 +0530

    conditions

--- console/JicJacJoe.java
+++ console/JicJacJoe.java
@@ -28,6 +28,59 @@ public class JicJacJoe {
         // store separately for easy checking
         int values[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         
+        while (true) {
+
+            input = read.readLine();
+            switch (input) {
+                case "1":
+                    if (board[1][1].equals("1"))
+                        board[1][1] = current;
+                    break;
+                case "2":
+                    if (board[1][1].equals("2"))
+                        board[1][] = current;
+                        
+                    break;
+                case "3":
+                    if (board[1][1].equals("3"))
+                        
+                    break;
+                case "4":
+                    if (board[1][1].equals("4"))
+                        
+                    break;
+                case "5":
+                    if (board[1][1].equals("5"))
+                        
+                    break;
+                case "6":
+                    if (board[1][1].equals("6"))
+                        
+                    break;
+                case "7":
+                    if (board[1][1].equals("7"))
+                        
+                    break;
+                case "8":
+                    if (board[1][1].equals("8"))
+                        
+                    break;
+                case "9":
+                    if (board[1][1].equals("9"))
+                        
+                    break;
+                case "X":
+                case "x":
+                    // showScore();
+                    return;
+                default:
+                    System.out.println(" Invalid input. Try again! ");
+
+            }
+            if (input.equals("Y") || input.equals("y"))
+                break;
+        }
+    }
 
     public static void clearScreen() {
         System.out.println("\033[H\033[2J");
