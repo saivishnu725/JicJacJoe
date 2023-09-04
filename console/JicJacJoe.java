4d631cc20ee092837814b1752ead3ad1a3f5a192
Sai Vishnu <saivishnu725@gmail.com>
2023-09-04 16:25:34 +0530

    basic variables

--- console/JicJacJoe.java
+++ console/JicJacJoe.java
@@ -1,5 +1,44 @@
+import java.io.*;
+
 public class JicJacJoe {
-    public static void main(String[] args) {
+    static String[][] board = {
+            { "|", "-", "-", "-", "-", "-", "|" },
+            { "|", "1", "|", "2", "|", "3", "|" },
+            { "|", "-", "|", "-", "|", "-", "|" },
+            { "|", "4", "|", "5", "|", "6", "|" },
+            { "|", "-", "|", "-", "|", "-", "|" },
+            { "|", "7", "|", "8", "|", "9", "|" },
+            { "|", "-", "-", "-", "-", "-", "|" },
+
+    };
+
+    public static void main(String[] args) throws IOException {
+        // input bs
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader read = new BufferedReader(reader);
+        // ask for clear screen or continue
+        boolean clearScreen = false;
+        // current player: X or Y
+        String current = "X";
+        // input var
+        String input = "";
+        // score for p1 and p2
+        int score[] = { 0, 0 };
+        // visuals of the board on screen
+        // store separately for easy checking
+        int values[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        
+
+    public static void clearScreen() {
         System.out.println("\033[H\033[2J");
+    }
+
+    public static void printBoard() {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
